131aacb368f230259f95667c2a500ba99bb933f3
HOUNKPE Mawulikplimi Roland <mawuliro2001@gmail.com>
2024-05-02 03:35:45 +0000

    the first day is done, writing comments, variables and types

---
+++ Day1/exo1.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+/**
+ * 1. Write a single line comment which says, comments can make code readable
+ * 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+ * 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+ * 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+ * 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+ * 6. Declare four variables without assigning values
+ * 7. Declare four variables with assigned values
+ * 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+ * 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+ * 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+ */
+// Comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/**
+ * Comments can make code readable,
+ * easy to reuse
+ * and informative
+ */
+
+let var1 = "Jesus is Love"
+let var2 = true
+let var3
+let var4 = null
+
+console.log(typeof(var1))
+console.log(typeof(var2))
+console.log(typeof(var3))
+console.log(typeof(var4))
+
+let firstName
+let lastName
+let isMarried
+let age
+
+let a = 1
+let b ="Power of Jesus Christ"
+let c = 2.5
+let d = false
+
+let myFirstName = "Roland"
+let myLastName = "HOUNKPE"
+let myMaritalStatus = "Single"
+let myAge = 23
+
+let myFirstName1 = "Roland", myLastName2 = "HOUNKPE", myMaritalStatus3 = "Single", myAge4 = 23
+
+let yourAge = 28
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old`)
